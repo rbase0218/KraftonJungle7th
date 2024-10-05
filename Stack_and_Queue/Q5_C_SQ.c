d4cc57612a40a35901465a6b6fa5ede57004b029
rbase0218 <rbase@naver.com>
2024-10-05 16:46:09 +0900

    Feat : Q5_C_SQ 문제 해결

--- Stack_and_Queue/Q5_C_SQ.c
+++ Stack_and_Queue/Q5_C_SQ.c
@@ -109,7 +109,12 @@ int main()
 
 void recursiveReverse(Queue *q)
 {
-/* add your code here */
+	if(isEmptyQueue(q))
+		return;
+
+	int num = dequeue(q);
+	recursiveReverse(q);
+	enqueue(q, num);
 }
 
 //////////////////////////////////////////////////////////////////
